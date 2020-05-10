51b1ba5937f1c742f70ca3e02fc4a6b664d31a84
nvaneck <nvaneck@stevens.edu>
2020-05-09 17:44:36 -0700

    Prelim Testing
    Familiarized ourselves with the associated graph converters and the graph reader. Decided to split up the initial testing of the 4 datasets. Added Single-threaded BFS Frontier Queue to list of algorithms in graph_reader_new/main.cpp

--- graph_reader_new/main.cpp
+++ graph_reader_new/main.cpp
@@ -21,6 +21,8 @@ int main(int args, char **argv)
     //You can implement your single threaded graph algorithm here.
     //like BFS, SSSP, PageRank and etc.
     
+    // Simple algorithm that prints neighbors and the associated weigths on those
+    // edges
     for(int i = 0; i < ginst->vert_count; i++)
     {
         int beg = ginst->beg_pos[i];
@@ -33,7 +35,56 @@ int main(int args, char **argv)
             std::cout<<"Money:"; std::cout<<ginst->weight[j]<<" ";
         }
         std::cout<<"\n";
-    } 
+    }
+     */
 
 	return 0;	
+}
+
+// Algorithm 1: Single-threaded BFS Frontier Queue
+// Provided in the associated docs
+char* BFSTraverse(graph<long, long, int, long, long, char>* ginst, int source){
+    int j,frontier = 0;
+    char* statusArray = new char[ginst->vert_count];
+    //Marks all verts as unvisited
+    for(int i = 0; i < ginst->vert_count; i++){
+        statusArray[i] = -1;
+    }
+    int* currFrontierQueue = new int[ginst->vert_count];
+    int* nextFrontierQueue = new int[ginst->vert_count];
+    int currFrontierSize = 0;
+    int nextFrontierSize = 0;
+    currFrontierQueue[currFrontierSize] = source;
+    currFrontierSize++;
+    //Mark Source as visited
+    statusArray[source] = 0;
+    int myFrontierIndex = 0;
+    int currLevel = 1;
+    while(true){
+        while(myFrontierIndex < currFrontierSize) {
+            frontier = currFrontierQueue[myFrontierIndex];
+            long int beg = ginst->beg_pos[frontier];
+            long int end = ginst->beg_pos[frontier+1];
+            
+            for(j = beg; j < end; j++){
+                if(statusArray[ginst->csr[j]] == -1){
+                    statusArray[ginst->csr[j]] = currLevel;
+                    nextFrontierQueue[nextFrontierSize] = ginst->csr[j];
+                    nextFrontierSize++;
+                }
+            }
+            myFrontierIndex++;
+        }
+        if(nextFrontierSize == 0){return statusArray;}
+        
+        //Swap current and next frontier queue
+        currFrontierSize = nextFrontierSize;
+        myFrontierIndex = 0;
+        nextFrontierSize = 0;
+        
+        int*temp = currFrontierQueue;
+        currFrontierQueue = nextFrontierQueue;
+        nextFrontierQueue = temp;
+        currLevel++;
+    }
 }
