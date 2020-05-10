db1caa9cccf40297303d7e89ddb72bf9139cf244
nvaneck <nvaneck@stevens.edu>
2020-05-09 18:48:32 -0700

    Algorithm 1 Implementation
    Added framework for Algorithm 1: Single-threaded BFS frontier queue

--- graph_reader_new/main.cpp
+++ graph_reader_new/main.cpp
@@ -1,49 +1,10 @@
 #include <iostream>
 #include "graph.h"
 
-int main(int args, char **argv)
-{
-	std::cout<<"Input: ./exe beg csr weight\n";
-	if(args!=4){std::cout<<"Wrong input\n"; return -1;}
-	
-	const char *beg_file=argv[1];
-	const char *csr_file=argv[2];
-	const char *weight_file=argv[3];
-	
-	//template <file_vertex_t, file_index_t, file_weight_t
-	//new_vertex_t, new_index_t, new_weight_t>
-	graph<long, long, /*int*/long, long, long,/* char*/long>
-	*ginst = new graph
-	<long, long, /*int*/long, long, long, /*char*/long>
-	(beg_file,csr_file,weight_file);
-    
-    //**
-    //You can implement your single threaded graph algorithm here.
-    //like BFS, SSSP, PageRank and etc.
-    
-    // Simple algorithm that prints neighbors and the associated weigths on those
-    // edges
-    for(int i = 0; i < ginst->vert_count; i++)
-    {
-        int beg = ginst->beg_pos[i];
-        int end = ginst->beg_pos[i+1];
-        std::cout<<i<<"'s neighor list: ";
-     //   std::cout<<i<<"'s outgoing money: ";
-        for(int j = beg; j < end; j++)
-        {
-            std::cout<<ginst->csr[j]<<" ";
-            std::cout<<"Money:"; std::cout<<ginst->weight[j]<<" ";
-        }
-        std::cout<<"\n";
-    }
-     */
-
-	return 0;	
-}
-
 // Algorithm 1: Single-threaded BFS Frontier Queue
 // Provided in the associated docs
-char* BFSTraverse(graph<long, long, int, long, long, char>* ginst, int source){
+char* BFSTraverse(graph<long, long, /*int*/ long, long, long, /*char*/ long>* ginst, int source){
+		double tm = wtime();
     int j,frontier = 0;
     char* statusArray = new char[ginst->vert_count];
     //Marks all verts as unvisited
@@ -65,7 +26,7 @@ char* BFSTraverse(graph<long, long, int, long, long, char>* ginst, int source){
             frontier = currFrontierQueue[myFrontierIndex];
             long int beg = ginst->beg_pos[frontier];
             long int end = ginst->beg_pos[frontier+1];
-            
+
             for(j = beg; j < end; j++){
                 if(statusArray[ginst->csr[j]] == -1){
                     statusArray[ginst->csr[j]] = currLevel;
@@ -75,16 +36,63 @@ char* BFSTraverse(graph<long, long, int, long, long, char>* ginst, int source){
             }
             myFrontierIndex++;
         }
-        if(nextFrontierSize == 0){return statusArray;}
-        
+        if(nextFrontierSize == 0){
+					std::cout<<"Runtime: "<<wtime()-tm<<" second(s).\n";
+					return statusArray;
+				}
+
         //Swap current and next frontier queue
         currFrontierSize = nextFrontierSize;
         myFrontierIndex = 0;
         nextFrontierSize = 0;
-        
+
         int*temp = currFrontierQueue;
         currFrontierQueue = nextFrontierQueue;
         nextFrontierQueue = temp;
         currLevel++;
     }
+}
+
+int main(int args, char **argv)
+{
+	std::cout<<"Input: ./exe beg csr weight\n";
+	if(args!=4){std::cout<<"Wrong input\n"; return -1;}
+
+	const char *beg_file=argv[1];
+	const char *csr_file=argv[2];
+	const char *weight_file=argv[3];
+
+	//template <file_vertex_t, file_index_t, file_weight_t
+	//new_vertex_t, new_index_t, new_weight_t>
+
+	//This setup is designed to work with the sample function
+	graph<long, long, /*int*/long, long, long,/* char*/long>
+	*ginst = new graph
+	<long, long, /*int*/long, long, long, /*char*/long>
+	(beg_file,csr_file,weight_file);
+
+    //**
+    //You can implement your single threaded graph algorithm here.
+    //like BFS, SSSP, PageRank and etc.
+
+    // Simple algorithm that prints neighbors and the associated weigths on those
+    // edges
+		/*
+    for(int i = 0; i < ginst->vert_count; i++)
+    {
+        int beg = ginst->beg_pos[i];
+        int end = ginst->beg_pos[i+1];
+        std::cout<<i<<"'s neighor list: ";
+     //   std::cout<<i<<"'s outgoing money: ";
+        for(int j = beg; j < end; j++)
+        {
+            std::cout<<ginst->csr[j]<<" ";
+            std::cout<<"Money:"; std::cout<<ginst->weight[j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+     */
+	char*statusArray = BFSTraverse(ginst,0);
+
+	return 0;
 }
