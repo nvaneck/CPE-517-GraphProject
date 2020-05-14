fa83d559eef6a7d61927a8b3adfe0430c97708a7
FLenge1996 <flper2@gmail.com>
2020-05-14 19:16:32 -0400

    Plz Work

--- graph_reader_new/main.cpp
+++ graph_reader_new/main.cpp
@@ -24,11 +24,10 @@ char* MultiStatusArrBFS(graph<long, long, int, long, long, char>* ginst,int sour
     int myFrontierCount = 0;
     int currLevel = 0; 
 
-    #pragma omp parallel num_threads(thread_count)
-        int mymyFrontierCount = 0;
-        int mycurrLevel = 0; 
+    #pragma omp parallel num_threads(thread_count) 
         while (true){ 
-
+            int mymyFrontierCount = 0;
+            int mycurrLevel = 0; 
             
                 //change while to for look for multithreading 
                 #pragma omp parallel for             
@@ -59,9 +58,10 @@ char* MultiStatusArrBFS(graph<long, long, int, long, long, char>* ginst,int sour
 }
 
 
-char* MultiFrontierQueueBFS(graph<long, long, /*int*/ long, long, long, /*char*/ long>* ginst, int source, int thread_count){
+char* MultiFrontierQueueBFS(graph<long, long, int, long, long, char>* ginst, int source, int thread_count){
     double tm = wtime();
     int j,frontier = 0;
+    omp_set_num_threads(thread_count);
  
     char* statusArray = new char[ginst->vert_count];
     //Marks all verts as unvisited
@@ -70,7 +70,8 @@ char* MultiFrontierQueueBFS(graph<long, long, /*int*/ long, long, long, /*char*/
     //}
 
     //Initializing table in parallell(MultiThreads)
-    #pragma omp parallel for (int i = 0; i < ginst->vert_count; ++i)
+    #pragma omp parallel for 
+    for(int i = 0; i < ginst->vert_count; ++i)
     {
         statusArray[i] = -1;
     }
@@ -85,36 +86,51 @@ char* MultiFrontierQueueBFS(graph<long, long, /*int*/ long, long, long, /*char*/
     int myFrontierIndex = 0;
     int currLevel = 1;
 
-    while(true){
-        while(myFrontierIndex < currFrontierSize) {
-            frontier = currFrontierQueue[myFrontierIndex];
-            long int beg = ginst->beg_pos[frontier];
-            long int end = ginst->beg_pos[frontier+1];
+    #pragma omp parallel num_threads(thread_count)
 
-            for(j = beg; j < end; j++){
-                if(statusArray[ginst->csr[j]] == -1){
-                    statusArray[ginst->csr[j]] = currLevel;
-                    nextFrontierQueue[nextFrontierSize] = ginst->csr[j];
-                    nextFrontierSize++;
+        while(true){
+            int myj,myfrontier = 0;
+            int* mycurrFrontierQueue = new int[ginst->vert_count];
+            int* mynextFrontierQueue = new int[ginst->vert_count];
+            int mycurrFrontierSize = 0;
+            int mynextFrontierSize = 0;
+            mycurrFrontierQueue[mycurrFrontierSize] = source;
+            mycurrFrontierSize++;
+            mystatusArray[source] = 0;
+            int mymyFrontierIndex = 0;
+            int mycurrLevel = 1;
+            
+            #pragma omp parallel for 
+            for(mymyFrontierIndex = 0; mymyFrontierIndex < mycurrFrontierSize; mymyFrontierIndex++) {
+                myfrontier = mycurrFrontierQueue[mymyFrontierIndex];
+                long int beg = ginst->beg_pos[myfrontier];
+                long int end = ginst->beg_pos[myfrontier+1];
+
+                for(myj = beg; myj < end; myj++){
+                    if(mystatusArray[ginst->csr[myj]] == -1){
+                        mystatusArray[ginst->csr[myj]] = mycurrLevel;
+                        mynextFrontierQueue[mynextFrontierSize] = ginst->csr[myj];
+                        mynextFrontierSize++;
+                    }
                 }
+                
             }
-            myFrontierIndex++;
+
+            if(mynextFrontierSize == 0){
+                    std::cout<<"Runtime: "<<wtime()-tm<<" second(s) for " <<thread_count<<" threads.\n";
+                        return mystatusArray;
+            }
+
+            //Swap current and next frontier queue
+            mycurrFrontierSize = mynextFrontierSize;
+            mymyFrontierIndex = 0;
+            mynextFrontierSize = 0;
+
+            int*temp = mycurrFrontierQueue;
+            mycurrFrontierQueue = mynextFrontierQueue;
+            mynextFrontierQueue = temp;
+            myurrLevel++;
         }
-        if(nextFrontierSize == 0){
-                    std::cout<<"Runtime: "<<wtime()-tm<<" second(s).\n";
-                    return statusArray;
-                }
-
-        //Swap current and next frontier queue
-        currFrontierSize = nextFrontierSize;
-        myFrontierIndex = 0;
-        nextFrontierSize = 0;
-
-        int*temp = currFrontierQueue;
-        currFrontierQueue = nextFrontierQueue;
-        nextFrontierQueue = temp;
-        currLevel++;
-    }
 }
 
 int main(int args, char **argv)
@@ -126,9 +142,9 @@ int main(int args, char **argv)
     const char *csr_file=argv[2];
     const char *weight_file=argv[3];
 
-    graph<long, long, /*int*/long, long, long,/* char*/long>
+    graph<long, long, int, long, long, char>
     *ginst = new graph
-    <long, long, /*int*/long, long, long, /*char*/long>
+    <long, long, int, long, long, char>
     (beg_file,csr_file,weight_file);
 
     char*statusArray = MultiStatusArrBFS(ginst,0, 1);
